44cdfbca5b8110432f35270ca0e32f3d8803658c
yongjin <yonggartong@naver.com>
2021-10-02 10:58:06 +0900

    count

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useState} from 'react'
 
 function App() {
+  const [count,setCount]=useState(0)
+
+  function minus(num) {
+    return setCount(num-1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <div>{count}</div>
+    <button onClick={()=>setCount(count+1)}>plus</button>
+    <button onClick={()=>minus(count)}>minus</button>
+  </div>
   );
 }
 
