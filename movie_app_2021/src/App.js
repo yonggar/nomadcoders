af972e08130dbe65f1bf5d86bb59f10507c7a39f
yongjin <yonggartong@naver.com>
2021-10-02 13:51:19 +0900

    useEffect

--- movie_app_2021/src/App.js
+++ movie_app_2021/src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {useState} from 'react'
 
 function App() {
   const [count,setCount]=useState(0)
+  const [isLoding,setLoding]=useState(true)
 
   function minus(num) {
     return setCount(num-1)
   }
+
+  useEffect(()=>{
+    setLoding(false)
+    console.log('마운트 될 때만')
+  },[count])
+
   return (
   <div>
+    <div>{isLoding ? 'Loding...':'we are ready'}</div>
     <div>{count}</div>
     <button onClick={()=>setCount(count+1)}>plus</button>
     <button onClick={()=>minus(count)}>minus</button>
